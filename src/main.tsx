d013acdfc614e8db908d2e7c7fa6caa415f2a276
tanishraj <tanishraj91@gmail.com>
2023-08-07 01:10:19 +0530

    refactor: husky pre commit changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import App from './App.tsx';
-import './index.css';
+import { App } from './App.tsx';
 import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
