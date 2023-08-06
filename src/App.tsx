7ec0f5fdbbadcdf5f2a57a77e70289e3d74c555b
Tanish Raj <tanishraj91@gmail.com>
2023-08-07 00:41:05 +0400

    Refactor/package update (#13)
    * refactor: package update
    
    * chore: adding package and setting
    
    * fix: updated package
    
    * fix: es lint setup is fixed
    
    * fix: package updated
    
    * refactor: husky pre commit changes
    
    * refactor: commit lint updates for husky
    
    * refactor: husky setup has been updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { Flex, Container, Heading, Text } from '@chakra-ui/react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
-    <>
-      <div>
+    <Container
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+      flexFlow='column'
+      height='100vh'>
+      <Flex
+        alignItems='center'
+        justifyContent='center'>
         <a
           href='https://vitejs.dev'
           target='_blank'>
@@ -27,21 +31,13 @@ function App() {
             alt='React logo'
           />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
+      </Flex>
+      <Heading>Vite + React</Heading>
+      <Text
+        as='p'
+        className='read-the-docs'>
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </Text>
+    </Container>
   );
 }
-
-export default App;
