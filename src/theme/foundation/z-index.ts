1524d7ea1a8712b942ac2e6f59f29d5c1727bb7a
tanishraj <tanishraj91@gmail.com>
2023-08-14 03:38:33 +0530

    chore: z index foundation value setup

---
+++ src/theme/foundation/z-index.ts
@@ -0,0 +1,11 @@
+export const zIndices = {
+  hide: -1,
+  auto: 'auto',
+  base: 0,
+  xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 16,
+  xl: 32,
+  '2xl': 40,
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,10 +3,12 @@ import { colors } from './foundation/colors';
 import { typography } from './foundation/typography';
 import { breakpoints } from './foundation/breakpoints';
 import { borderRadius } from './foundation/border-radius';
+import { zIndices } from './foundation/z-index';
 
 export const theme = extendTheme({
   colors,
   breakpoints,
+  zIndices,
   ...typography,
   ...borderRadius,
 });
