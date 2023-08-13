06e6adb2983fec078aa8c44b0d0f1f67bbf4d9b3
tanishraj <tanishraj91@gmail.com>
2023-08-14 02:29:27 +0530

    chore: typography rules setup for the starter template

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,8 @@
 import { extendTheme } from '@chakra-ui/react';
 import { colors } from './foundation/colors';
+import { typography } from './foundation/typography';
 
 export const theme = extendTheme({
   colors,
+  ...typography,
 });
