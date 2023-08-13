6ce574065faa6065f1e6ca1bf6a53ee38b858ae0
Tanish Raj <tanishraj91@gmail.com>
2023-08-14 02:20:08 +0400

    chore: spacing foundation value setup (#24)

---
+++ src/theme/foundation/spacing.ts
@@ -0,0 +1,19 @@
+import { toRem } from '../utils/to-rem';
+
+export const space = {
+  '4xs': toRem(8),
+  '3xs': toRem(12),
+  '2xs': toRem(16),
+  xs: toRem(20),
+  sm: toRem(24),
+  md: toRem(28),
+  lg: toRem(32),
+  xl: toRem(36),
+  '2xl': toRem(40),
+  '3xl': toRem(48),
+  '4xl': toRem(60),
+  '5xl': toRem(64),
+  '6xl': toRem(80),
+  '7xl': toRem(120),
+  '8xl': toRem(160),
+};
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -4,11 +4,13 @@ import { typography } from './foundation/typography';
 import { breakpoints } from './foundation/breakpoints';
 import { borderRadius } from './foundation/border-radius';
 import { zIndices } from './foundation/z-index';
+import { space } from './foundation/spacing';
 
 export const theme = extendTheme({
   colors,
   breakpoints,
   zIndices,
+  space,
   ...typography,
   ...borderRadius,
 });
