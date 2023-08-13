7a1c689894e44b65fe0bf6f792c412c7fa9f5d9d
Tanish Raj <tanishraj91@gmail.com>
2023-08-14 01:43:19 +0400

    Chore/breakpoints setup (#20)
    * chore: breakpoints as per tailwind standard
    
    * chore: importing breakpoints to theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 import { colors } from './foundation/colors';
 import { typography } from './foundation/typography';
+import { breakpoints } from './foundation/breakpoints';
 
 export const theme = extendTheme({
   colors,
+  breakpoints,
   ...typography,
 });
