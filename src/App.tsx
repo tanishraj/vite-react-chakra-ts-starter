457a3f8fdbf201f9ceb028ac01d784e8dc864dc1
tanishraj <tanishraj91@gmail.com>
2023-08-26 02:02:38 +0530

    feat: custom font support

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Layout } from './lib/layout';
 import { Routings } from './lib/router/routings';
 import { theme } from './lib/styles/theme';
+import '../src/lib/styles/globals/globals.css';
 
 export function App() {
   return (
